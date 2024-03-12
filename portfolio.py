b033e8880f3cb8a23455bd4caa734f0663f17136
Rocio Palacin <you@example.com>
2024-03-11 22:02:54 -0300

    adding franc class

--- portfolio.py
+++ portfolio.py
@@ -7,11 +7,27 @@ class Dollar:
         
         def __eq__(self,dollar):
             return self.__amount == dollar.__amount
+        
+
+class Franc:
+        def __init__(self, amount):
+            self.__amount = amount
+
+        def times(self, multiplier):
+            return Franc(self.__amount * multiplier)
+        
+        def __eq__(self,franc):
+            return self.__amount == franc.__amount
           
-def test_franc_multiplication():
+def test_multiplication():
         five = Dollar(amount=5)
         assert Dollar(amount=10) == five.times(multiplier=2)
         assert Dollar(amount=15) == five.times(multiplier=3) 
+
+def test_franc_multiplication():
+        five = Franc(amount=5)
+        assert Franc(amount=10) == five.times(multiplier=2)
+        assert Franc(amount=15) == five.times(multiplier=3) 
 
 def test_equality():
         assert Dollar(3)== Dollar(3)
