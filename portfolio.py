8d8cee93103d04a75accccc5bb920c0b53fc0264
Rocio Palacin <you@example.com>
2024-03-11 21:22:42 -0300

    testing object is not changing

--- portfolio.py
+++ portfolio.py
@@ -11,7 +11,11 @@ def test_multiplication():
     def test_multiplication():
         # test that you can multiply a Dollar by a number and get the right amount.
         five = Dollar(amount=5)
-
         # a esos cinco usd le vamos a implementar el método multiplicacion
+        #este paso no es muy bueno porque modifica el objeto original
         five.times(multiplier=2)
         assert 10 == five.amount
+        #si el 5 no cambió, al multiplicarlo por 3 debería ser 15!
+        five.times(multiplier=3)
+        assert 15 == five.amount
+        
